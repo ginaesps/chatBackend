06d75f3f6ec385a169da20967bfdf64b883b1d6e
ginaesps <respinosa7@ucol.mx>
2023-02-10 10:47:03 -0600

    dataBaseConfig file creation

--- src/main/java/com/chatapp/chatserver/ChatserverApplication.java
+++ src/main/java/com/chatapp/chatserver/ChatserverApplication.java
@@ -2,11 +2,14 @@ package com.chatapp.chatserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath: ./../resources/application.yml")
 public class ChatserverApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ChatserverApplication.class, args);
 	}
 
---
+++ src/main/java/com/chatapp/chatserver/config/DatabaseConfig.java
@@ -0,0 +1,35 @@
+package com.chatapp.chatserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return dataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
